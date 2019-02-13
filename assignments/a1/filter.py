ea8e9ed50b1d0d17919f811f1d2d3f4a173ea3f4
Roney Thomas <roneythomas6@gmail.com>
2019-02-13 17:02:28 -0500

    Added another exception to be safe

--- assignments/a1/filter.py
+++ assignments/a1/filter.py
@@ -165,7 +165,7 @@ class DurationFilter(Filter):
                         calls.append(call)
             else:
                 return data
-        except (ValueError, IndexError):
+        except (ValueError, IndexError, Exception):
             return data
 
         # if not calls:
